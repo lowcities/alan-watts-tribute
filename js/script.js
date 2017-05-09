b3f96d1ded9cbed528fbb9af912f8f003de2cab7
David Low <musumashae@gmail.com>
2017-05-09 16:55:54 -0400

    reformatted lead paragraphs and second photo and eliminated text scrolling animation

--- js/script.js
+++ js/script.js
@@ -7,36 +7,36 @@ split = function(element) {
   $(element).html(text);
 };
 
-textify = function(element, method, delay) {
-  split(element);
-  $(element + ' span').css('opacity', '0')
-  $(element + ' span').css('position', 'relative');
-  in_speed = 10;
-  count = 0;
-  setTimeout(function() {
-    count = 0;
-    $(element + ' span').each(function() {
-      if (method === 'fade') {
-        $(this).delay(0 + in_speed * count).animate({
-          opacity: '1'
-        }, 200);
-      } else if (method === 'bounce') {
-        $(this).delay(0 + in_speed * count).animate({
-          opacity: '1',
-          'top': '-4px'
-        }, 220, 'easeOutCubic');
-        $(this).delay(0 + in_speed * count / 4).animate({
-          opacity: '1',
-          'top': '0px'
-        }, 220);
-      }
-      count++;
-    });
-  }, delay);
-};
-
-textify('.content1', 'fade', 500);
-textify('.p2', 'fade', 4000);
+//textify = function(element, method, delay) {
+//  split(element);
+//  $(element + ' span').css('opacity', '0')
+//  $(element + ' span').css('position', 'relative');
+//  in_speed = 10;
+//  count = 0;
+//  setTimeout(function() {
+//    count = 0;
+//    $(element + ' span').each(function() {
+//      if (method === 'fade') {
+//        $(this).delay(0 + in_speed * count).animate({
+//          opacity: '1'
+//        }, 200);
+//      } else if (method === 'bounce') {
+//        $(this).delay(0 + in_speed * count).animate({
+//          opacity: '1',
+//          'top': '-4px'
+//        }, 220, 'easeOutCubic');
+//        $(this).delay(0 + in_speed * count / 4).animate({
+//          opacity: '1',
+//          'top': '0px'
+//        }, 220);
+//      }
+//      count++;
+//    });
+//  }, delay);
+//};
+//
+//textify('.content1', 'fade', 500);
+//textify('.p2', 'fade', 4000);
 
 $(window).scroll(function() {
   $(".image1").css("opacity", 1 - $(window).scrollTop() / 300);
